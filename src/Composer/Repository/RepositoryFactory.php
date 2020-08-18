64721b55926bc9f8308f0b887ffdb6540a002344
Oleg Andreyev <oleg@andreyev.lv>
2020-08-19 00:46:49 +0300

    bitbucket server >= v6.4.0

--- src/Composer/Repository/RepositoryFactory.php
+++ src/Composer/Repository/RepositoryFactory.php
@@ -129,6 +129,7 @@ class RepositoryFactory
         $rm->setRepositoryClass('hg', 'Composer\Repository\VcsRepository');
         $rm->setRepositoryClass('artifact', 'Composer\Repository\ArtifactRepository');
         $rm->setRepositoryClass('path', 'Composer\Repository\PathRepository');
+        $rm->setRepositoryClass('bitbucket-server', 'Composer\Repository\VcsRepository');
 
         return $rm;
     }
